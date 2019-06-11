6a4d9ee289f028575fe79192820113e1ad5e69b4
Vincent Vandenborne <vncvdb@gmail.com>
2019-06-11 12:45:42 +0200

    completed models with validations and dependencies

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,7 +1,7 @@
 class Offer < ApplicationRecord
   # Sets the specific name of the foreign key to the volunteer_id like in the
   # migration above
-
+  validates :talent, uniqueness: { scope: :volunteer }
   belongs_to :volunteer, class_name: :User, foreign_key: :volunteer_id
   belongs_to :talent
   # we can now create Offer.create(volunteer: "")
