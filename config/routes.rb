b7c1f401da9403f361b08af80645ba570db13de6
Vincent Vandenborne <vncvdb@gmail.com>
2019-06-11 15:33:20 +0200

    added Invitations show view

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   resources :offers, only: [:new, :create, :edit, :update]
   resources :offers, only: [:index, :destroy]
+  resources :invitations, only: [:create, :show] # we won't need a show page since we will show the results in the dashboard
+
+  #create dashboard controller with index
 end
