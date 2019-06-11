149f41f0a28593b616ad7410fbef56d1bdccb13e
Vincent Vandenborne <vncvdb@gmail.com>
2019-06-11 13:41:32 +0200

    created seeds and completed model validation

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -1,5 +1,7 @@
 class Invitation < ApplicationRecord
-  validates :offer, uniqueness: { scope: :host }
+  validates :offer, presence: true, uniqueness: { scope: :host }
+  validates :host_id, presence: true
+  validates :title, presence: true
   belongs_to :host, class_name: :User, foreign_key: :host_id
   belongs_to :offer
 end
