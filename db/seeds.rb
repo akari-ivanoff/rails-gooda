b5f88ce80eee06ac03842b34ce91ee30d4b0216c
Anne Hofmann <annemff@yahoo.de>
2019-06-11 19:17:33 +0200

    Offers index draft created

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+meg = User.create(first_name: 'Meg',
+                last_name: 'Griffin',
+                location: 'Malmö',
+                email: 'marge@griffin.com',
+                password: '111111',
+                bio: "has a hat")
+
+marge = User.create(first_name: 'Marge',
+                last_name: 'Simpson',
+                location: 'Almhult',
+                email: 'marge@simpson.com',
+                password: '111111',
+                bio: "very tidy")
+
 homer = User.create(first_name: 'Homer',
                 last_name: 'Simpson',
                 location: 'Almhult',
@@ -14,18 +28,38 @@ homer = User.create(first_name: 'Homer',
 
 peter = User.create(first_name: 'Peter',
                 last_name: 'Griffin',
-                location: 'Almhult',
+                location: 'Malmö',
                 email: 'peter@griffin.com',
                 password: '111111',
                 bio: "is not very competent")
 
+
+
 talents = ['Cooking & Baking', 'Gardening & Plants', 'Pet sitting', 'Teach & learn', 'Repairs around the house', 'Parenting and Caretaking']
 # ['Carpentry & Construction', 'Cleaning', 'Decoration', 'Deep Clean', 'Delivery', 'Electrician', 'Errands', 'Event Staffing', 'Executive Assistant', 'Furniture Assembly','IKEA Assembly', 'Laundry and Ironing', 'Lift & Shift Furniture', 'Minor Home Repairs', 'Mounting', 'Moving Help', 'Office Administration', 'Organization', 'Packing & Unpacking', 'Painting', 'Pet Sitting', 'Plumbing', 'Sewing', 'Shopping', 'Waiting in Line', 'Window Cleaning', 'Removal']
+talentobjects =[]
 talents.each do |talent|
-  Talent.create(title: talent)
+  talentobjects << Talent.create(title: talent)
 end
+puts talentobjects[0].title
+homerfirstoffer = Offer.create(volunteer: homer, comment: "Lorem ispum Lorem ispum Lorem", talent: talentobjects[0])
+homersecondoffer = Offer.create(volunteer: homer, comment: "Lorem ispum Lorem ispum Lorem ispum Lorem ispum Lorem ispum", talent: talentobjects[1])
+homerthirdoffer = Offer.create(volunteer: homer, comment: "Lorem ispum Lorem ispum ", talent: talentobjects[2])
 
-homerfirstoffer = Offer.create(volunteer: homer, talent: Talent.last)
-homersecondoffer = Offer.create(volunteer: homer, talent: Talent.first)
+megfirstoffer = Offer.create(volunteer: meg, comment: "Lorem ispum Lorem ispum Lorem ispum Lorem ispum Lorem ispum Lorem ispum Lorem ispum Lorem ispum Lorem ispum Lorem ispum", talent: talentobjects[0])
+megsecondoffer = Offer.create(volunteer: meg, comment: "Lorem ispum Lorem ispum Lorem ispum ispum", talent: talentobjects[1])
+megthirdoffer = Offer.create(volunteer: meg, comment: "Lorem ispum Lorem ispum Lorem ispum Lorem ispum Lorem ispum Lorem ispum Lorem ispum Lorem ispum Lorem ispum Lorem ispum", talent: talentobjects[2])
+
+
+margefirstoffer = Offer.create(volunteer: marge, comment: "Lorem ispum Lorem ispum Lorem ispum Lorem ispum Lorem ispum", talent: talentobjects[0])
+margesecondoffer = Offer.create(volunteer: marge, comment: "Lorem ispum Lorem ispum Lorem ispum Lorem ispum Lorem ispum Lorem ispum Lorem ispum Lorem ispum Lorem ispum Lorem ispum", talent: talentobjects[1])
+margethirdoffer = Offer.create(volunteer: marge, comment: "Lorem ispum Lorem ispum ", talent: talentobjects[2])
+margefourthoffer = Offer.create(volunteer: marge, comment: "Lorem ispum Lorem ispum Lorem ispum Lorem ispum Lorem ispum", talent: talentobjects[3])
+
 Invitation.create(title:'mow lawn', description:'my lawn is full of long grass, i would like it to be shorter', host: peter, offer: homerfirstoffer)
 Invitation.create(title: 'paint cat', description: 'my cat is too white', host: peter, offer: homersecondoffer)
+Invitation.create(title: 'paint lawn', description: 'you know', host: peter, offer: megfirstoffer)
+Invitation.create(title: 'mow cat', description: 'not that difficult', host: homer, offer: margefirstoffer)
+Invitation.create(title: 'wait for cat', description: 'be patient', host: homer, offer: margesecondoffer)
+Invitation.create(title: '', description: '', host: homer, offer: margethirdoffer)
+Invitation.create(title: '', description: '', host: peter, offer: margefourthoffer)
